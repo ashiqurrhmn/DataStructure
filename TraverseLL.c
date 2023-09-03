f7294c65c14857ae427538a31aff41357be1b8e4
Md. Ashiqur Rahman <138354564+ashiqurrhmn@users.noreply.github.com>
2023-09-04 03:23:52 +0600

    Add files via upload

---
+++ TraverseLL.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node
+{
+    int data;
+    struct node *link;
+} Node;
+int main()
+{
+    Node *head = NULL;
+    head = (Node *)malloc(sizeof(Node));
+    head->data = 45;
+    head->link = NULL;
+
+    Node *current = NULL;
+    current = (Node *)malloc(sizeof(Node));
+    current->data = 98;
+    current->link = NULL;
+    head->link = current;
+
+    current = (Node *)malloc(sizeof(Node));
+    current->data = 3;
+    current->link = NULL;
+    head->link->link = current;
+
+    printData(head);
+
+    return 0;
+}
+void printData(Node *head)
+{
+    if(head == NULL)
+        printf("The Linked List is empty\n");
+    while(head != NULL)
+    {
+        printf("%d -> ", head->data);
+        head = head->link;
+    }
+    printf("NULL");
+
+}
+
+
