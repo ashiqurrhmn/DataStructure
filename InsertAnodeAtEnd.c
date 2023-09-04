7be7abf2ab184c1caaa4d34f55925303029730ee
Md. Ashiqur Rahman <138354564+ashiqurrhmn@users.noreply.github.com>
2023-09-04 22:40:32 +0600

    Add files via upload

---
+++ InsertAnodeAtEnd.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node
+{
+    int data;
+    struct node *link;
+} Node;
+int main()
+{
+    Node *head = NULL;
+    head = (Node *)malloc(sizeof(Node));
+    head->data = 45;
+    head->link = NULL;
+
+    Node *current = NULL;
+    current = (Node *)malloc(sizeof(Node));
+    current->data = 98;
+    current->link = NULL;
+    head->link = current;
+
+    current = (Node *)malloc(sizeof(Node));
+    current->data = 3;
+    current->link = NULL;
+    head->link->link = current;
+
+    add_at_end(head, 67);
+
+    printData(head);
+
+    return 0;
+}
+void add_at_end(Node *head, int data)
+{
+    Node *ptr, *temp;
+    ptr = head;
+    temp = malloc(sizeof(Node));
+    temp->data = data;
+    temp->link = NULL;
+
+    while(ptr->link != NULL)
+    {
+        ptr = ptr->link;
+    }
+    ptr->link = temp;
+}
+void printData(Node *head)
+{
+    if(head == NULL)
+        printf("The Linked List is empty\n");
+    while(head != NULL)
+    {
+        printf("%d -> ", head->data);
+        head = head->link;
+    }
+    printf("NULL");
+
+}
