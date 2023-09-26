82f081ed67a47e2214b93c7afeb25e333934b80f
Md. Ashiqur Rahman <138354564+ashiqurrhmn@users.noreply.github.com>
2023-09-26 09:44:58 +0600

    Update StackInLL.c

--- StackInLL.c
+++ StackInLL.c
@@ -1,62 +1,63 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
 
-typedef struct Node {
+typedef struct Node{
     int data;
     struct Node* next;
-} Node;
+}Node;
 
-Node* top = NULL;
+Node *top=NULL;
 
 void push(int data) {
     Node* temp = (Node*)malloc(sizeof(Node));
-    if (temp == NULL) {
-        printf("Stack is full!!\n");
-        return; // Return early on memory allocation failure.
+    if(temp == NULL) {
+        printf("Stack is full!!");
     }
     temp->data = data;
     temp->next = top;
     top = temp;
-    printf("push is called..... ");
+    printf("push is called ");
 }
 
 void pop() {
-    if (top == NULL) {
-        printf("Stack is Empty! Can't pop\n");
+    if(top == NULL) {
+        printf("Stack is Empty! Can't pop");
         return;
     }
     Node* temp = top;
     top = top->next;
     free(temp);
-    printf("pop is called.... ");
+    printf("pop is called ");
 }
 
-int peek() {
-    if (top != NULL) {
+int peek(){
+    if(top != NULL) {
         return top->data;
-    } else {
+    }else {
         return INT_MIN;
     }
+
 }
 
-void display() {
-    Node* temp = top;
-    printf("Stack: ");
+void display(){
+    Node * temp = top;
+    printf("Stack:");
 
-    while (temp != NULL) {
-        printf("%d ", temp->data);
+    while(temp !=NULL) {
+        printf("%d ",temp->data);
         temp = temp->next;
     }
     printf("\n");
 }
 
 void displayRev(Node* p) {
-    if (p == NULL) {
-        return; // Base case: stop recursion when p is NULL.
+    if(p == NULL) {
+        printf("Reverse: ");
+        return;
     }
     displayRev(p->next);
-    printf("%d ", p->data);
+    printf("%d ",p->data);
 }
 
 int main() {
@@ -68,15 +69,9 @@ int main() {
     display();
     push(7);
     display();
-    displayRev(top); // Print the stack elements in reverse order.
+    displayRev(top);
     printf("\n");
 
-    printf("Top = %d\n", peek());
+    printf("Top = %d\n",peek());
 
-    // Clean up the remaining elements in the stack before exiting.
-    while (top != NULL) {
-        pop();
-    }
-
-    return 0;
 }
